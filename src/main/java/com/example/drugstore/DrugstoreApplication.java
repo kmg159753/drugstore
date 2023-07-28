88d59de4e1c2eb7d361610736a80ae2643b11413
KIM MIN GYU <mingyugim584@gmail.com>
2023-07-28 20:51:16 +0900

    Initial commit

---
+++ src/main/java/com/example/drugstore/DrugstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.drugstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrugstoreApplication.class, args);
+    }
+
+}
